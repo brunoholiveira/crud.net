822c5061c97359b067f76db38148f9ce26ca8139
Bruno Henrique de Oliveira <124120411+brunoholiveira@users.noreply.github.com>
2024-12-04 16:17:29 -0300

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebCRUDmvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
